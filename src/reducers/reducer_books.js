69386f7add4c239920a4d45520776fe6a126bd39
DrewStock <andrewdstock@gmail.com>
2017-12-19 11:39:53 -0800

    setting up first reducer (reducer_books.js) and container component (book_list.js)

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'On The Road'},
+        { title: 'The Motorcycle Diaries'},
+        { title: 'The Mysteries of Pittsburgh'},
+        { title: 'The Dharma Bums'}
+    ]
+}

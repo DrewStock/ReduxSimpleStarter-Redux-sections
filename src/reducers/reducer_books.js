08d2d9e12963a74e8f2e434c24e918b06e97d6fb
DrewStock <andrewdstock@gmail.com>
2017-12-19 17:42:56 -0800

    added book_detail container and selectBook action

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'On The Road'},
-        { title: 'The Motorcycle Diaries'},
-        { title: 'The Mysteries of Pittsburgh'},
-        { title: 'The Dharma Bums'}
+        { title: 'On The Road', pages: 199},
+        { title: 'The Motorcycle Diaries', pages: 300},
+        { title: 'The Mysteries of Pittsburgh', pages: 250},
+        { title: 'The Dharma Bums', pages: 175}
     ]
 }
